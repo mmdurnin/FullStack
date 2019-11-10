9851a0b28ab0ee12b6e1597cbbb34f0aa1e62d15
your name here <maureenmdurnin@gmail.com>
2019-11-09 22:52:25 -0800

    restaurant show page

--- frontend/components/restaurants/restaurant_index.jsx
+++ frontend/components/restaurants/restaurant_index.jsx
@@ -1,36 +1,52 @@
 import React from 'react';
 import SearchFormContainer from '../home/search_bar/search_form_container'
-
+import RestaurantIndexItem from './restaurant_index_item';
 
 class RestaurantIndex extends React.Component{
     constructor(props){
         super(props)
-        console.log(this.props)
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchRestaurants(this.props.cityId)
     }
 
     render(){
         if (this.props.restaurants === []) return null
-
-        // const backgroundStyle = `background-image: image_url("/cities/searchbar_cities/searchbar_${cityId}.jpg");`
         const backgroundStyle = {backgroundImage: 'url(' + window.searchBarCities[this.props.cityId] + ')'};
-        
 
         return(
-            <div>
+            <div className="restaurant-index-window-container">
+
                 <div className="index-search-bar-container" style={backgroundStyle}>
                     <div className="index-search-bar-form">
                         <SearchFormContainer />
                     </div>
                 </div>
-                {
-                    this.props.restaurants.map((el, i) => {
-                        return <li key={i}>{el.name}</li>
-                    })
-                }
+
+                <div className="restaurant-index-page-container">
+
+                    <div className="restaurant-index-navbar-container">
+                        <div>Not Sticky?</div>
+                        <div className="sticky-navbar">
+                            <div className="price-filter-container">
+                                Price:
+                            </div>
+                            <div className="cusine-filter">
+                                Cuisine:
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="restaurant-list-container">
+                        {
+                            this.props.restaurants.map((el, i) => {
+                                return <RestaurantIndexItem key={i} restaurant={el} />
+                            })
+                        }
+                    </div>
+
+                </div>
             </div>
         )
     }
---
+++ frontend/components/restaurants/restaurant_index_item.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class RestaurantIndexItem extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        const translatePricing = {
+            1: '$',
+            2: '$$',
+            3: '$$$',
+            4: '$$$$',
+            5: '$$$$$'
+        }
+
+        const starText = {
+            1: "",
+            1.5: "",
+            2: "",
+            2.5: "",
+            3: "Up and coming",
+            3.5: "Up and coming",
+            4: "Pretty good!",
+            4.5: "So good!",
+            5: "Superb!"
+
+        }
+
+        const rating = 4.5; // replace later with rating props
+        const stars = [];
+        for (let i = 0; i < Math.floor(rating); i++) {
+            stars.push(<img key={i} src={window.fullStar} />)
+        }
+        if (rating % 1 != 0) {
+            stars.push(<div key={6} className="last-star"><img src={window.halfStar} /></div>)
+        }
+
+        return(
+            <div className="restaurant-index-item-container">
+
+                <div className="index-item-image-container">
+                    <Link to={`/restaurants/${this.props.restaurant.id}`} ><img src={this.props.restaurant.image}/></Link>
+                </div>
+
+                <div className="index-item-overview-container">
+                    <div className="restaurant-item-header">
+                        <Link to={`/restaurants/${this.props.restaurant.id}`} ><h3 className="restaurantTitle">{this.props.restaurant.name}</h3></Link>
+                        <h4 className={'index-restaurant-item-pricing'}>{translatePricing[this.props.restaurant.pricing]}</h4>
+                    </div>
+
+                    <div className="overview-rating">
+                        <div className="rating-subtext">People are saying: {starText[rating]}</div>
+                        <div className="rating-stars">{stars}</div>
+                    </div>
+
+                    <h4 className="index-item-neighborhood">Neighborhood: {this.props.restaurant.neighborhood}</h4>
+
+                    <h4 className="index-item-overview-bookstat">Booked 0 times today</h4>
+
+                    <div className="buttons-container">
+                        <button>5:00PM</button>
+                        <button>5:00PM</button>
+                        <button>5:00PM</button>
+                        <button>5:00PM</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default RestaurantIndexItem;
