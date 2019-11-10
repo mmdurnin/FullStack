b8319614f6e3029f80d855498b7069cc55398a9d
your name here <maureenmdurnin@gmail.com>
2019-11-10 13:10:26 -0800

    restaurant show page

--- frontend/components/restaurants/restaurant_detail.jsx
+++ frontend/components/restaurants/restaurant_detail.jsx
@@ -4,6 +4,11 @@ class RestaurantDetail extends React.Component{
     constructor(props){
         super(props)
 
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
     }
 
     componentDidMount(){
@@ -20,7 +25,49 @@ class RestaurantDetail extends React.Component{
         if (this.props.restaurant == null) return null;
 
         return(
-            <div>{this.props.restaurant.name}</div>
+            <div className="restaurant-show-window">
+
+                <div className="restaurant-show-header">
+                </div>
+
+                <div className="restaurant-show-page">
+
+                    <div className="restaurant-show-main-container">
+                        <div className="show-main-options">
+                            <div>Summary</div>
+                            <div>Photos</div>
+                            <div>General Info</div>
+                            <div>Reviews</div>
+                        </div>
+                        <div className="show-main-content-container">
+                            {this.props.restaurant.name}
+                        </div>
+                    </div>
+
+                    <div className="restaurant-show-nav-container">
+                        <div className="restaurant-show-reservation-container">
+                            <div className="reservation-container-header">Make a reservation</div>
+                            <div className="reservation-container-content">
+                                <form onSubmit={this.handleSubmit}>
+
+                                    <div className="reservation-form-section">
+                                        <label>Party Size</label>
+                                    </div>
+
+                                    <div className="reservation-form-section res-datetime">
+                                        <label>Date</label>
+                                        <label>Time</label>
+                                    </div>
+
+                                    <div className="reservation-form-section">
+                                        <input type="submit" value="Confirm Reservation"/>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
