dcc3767729d78b42422859978251929b03988094
your name here <maureenmdurnin@gmail.com>
2019-11-12 15:56:20 -0800

    user profile reservations more formatting

--- frontend/components/reservations/reservation_detail.jsx
+++ frontend/components/reservations/reservation_detail.jsx
@@ -11,6 +11,24 @@ class ReservationDetail extends React.Component {
 
     render() {
         console.log(this.props)
+
+        const timeOptions = ["12:00 PM", "12:30PM"];
+        let time;
+        for (let i = 1; i < 12; i++) {
+            for (let j = 0; j < 2; j++) {
+                time = "";
+                time = time.concat(`${i}`)
+
+                if (j === 0) {
+                    time = time.concat(":00 PM")
+                } else {
+                    time = time.concat(":30 PM")
+                }
+                
+                timeOptions.push(time)
+            }
+        }
+        
         return(
             <div>
                 <div className="user-profile-res-restaurant-name">{this.props.reservation.restaurant_name}</div>
@@ -19,9 +37,12 @@ class ReservationDetail extends React.Component {
                         <div className="user-profile-res-partyof">{this.props.user.name}, party of: {this.props.reservation.num_guests}</div>
                         <div className="user-profile-res-contact">Retaurant contact info:</div>
                         <div className="user-profile-res-contact">{this.props.reservation.restaurant_phone}</div>
+                        <div className="user-profile-res-contact">{this.props.reservation.restaurant_address}</div>
+                        <div className="user-profile-res-contact">{this.props.reservation.restaurant_neighborhood}</div>
                     </div>
                     <div className="user-profile-reservation-date">
-                        <div>Reservation: {this.props.reservation.starts_at_date}</div>
+                        <div>Reservation:</div>
+                        <div>{this.props.reservation.starts_at_date}</div>
                         <div>{this.props.reservation.starts_at_time}</div>
                     </div>
                 </div>
