e365e881f6468e9875c0ef350531f6c9ccdc3218
your name here <maureenmdurnin@gmail.com>
2019-11-14 11:20:41 -0800

    scroll to top

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from './scroll_to_top';
 import App from './app';
 
 const Root = ({ store }) => (
     <Provider store={store}>
         <HashRouter>
-            <App />
+            <ScrollToTop>
+                <App />
+            </ScrollToTop>
         </HashRouter>
     </Provider>
 )
