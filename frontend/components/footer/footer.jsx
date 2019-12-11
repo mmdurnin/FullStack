2013209368db75195d52a02aa3894f9be988a022
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-10 18:46:37 -0800

    add social icons with links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Footer = () => (
     <div className="footer-main">
-
+        <Link to="https://angel.co/maureen-durnin?public_profile=1"><i class="footer-icon-circle fab fa-angellist"></i></Link>
+        <Link to="https://www.linkedin.com/in/maureen-durnin-19b73a198/"><i class="footer-icon-circle fab fa-linkedin-in"></i></Link>
+        <Link to="https://github.com/mmdurnin"><i class="footer-icon fab fa-github"></i></Link>
+        <Link to="maureendurnin.com" ><i class="footer-icon fas fa-user-circle"></i></Link>
     </div>
 )
 
